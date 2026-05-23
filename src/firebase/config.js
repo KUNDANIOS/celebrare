21148b4695c3654bb38dffd931fa1669e627ad74
KUNDANIOS <kundan220f@gmail.com>
2026-05-24 03:01:50 +0530

    fix: remove exposed firebase keys, use env variables

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,14 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
-
 const firebaseConfig = {
-  apiKey: "AIzaSyAi_sWRPJumPrrlKlmOYiJjjxfc1WJlHBg",
-  authDomain: "react-auth-project-8f346.firebaseapp.com",
-  projectId: "react-auth-project-8f346",
-  storageBucket: "react-auth-project-8f346.firebasestorage.app",
-  messagingSenderId: "859313295963",
-  appId: "1:859313295963:web:dd2260186cf1a716c9657e",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
